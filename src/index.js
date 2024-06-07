4d6fdf78ef9634a1ac90934939b7b153ee7ee99c
l3x-fx <kekskiller@email.com>
2024-06-07 11:37:54 +0200

    bugfixes

--- src/index.js
+++ src/index.js
@@ -1,64 +1,62 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import { createRoot } from "react-dom"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import reportWebVitals from "./reportWebVitals"
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-import './index.css';
-import App from './App';
+import "./index.css"
+import App from "./App"
 
-import { PollList } from './features/polls/PollList';
-import { NewPoll } from './features/polls/NewPoll';
-import { Login } from './features/auth/Login';
-import { Leaderboard } from './features/users/Leaderboard';
-import { PollDetails } from './features/polls/PollDetails';
-import { Error } from './app/Error';
-import { GeneralError } from './app/GeneralError';
+import { PollList } from "./features/polls/PollList"
+import { NewPoll } from "./features/polls/NewPoll"
+import { Login } from "./features/auth/Login"
+import { Leaderboard } from "./features/users/Leaderboard"
+import { PollDetails } from "./features/polls/PollDetails"
+import { Error } from "./app/Error"
+import { GeneralError } from "./app/GeneralError"
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <App />, 
+    path: "/",
+    element: <App />,
     errorElement: <GeneralError />,
-    children:[
+    children: [
       {
         index: true,
         element: <PollList />,
       },
       {
-        path:"/login",
-        element:<Login /> 
-      },    
+        path: "/login",
+        element: <Login />,
+      },
       {
-        path:"/add",
-        element:<NewPoll />, 
+        path: "/add",
+        element: <NewPoll />,
       },
       {
         path: "/leaderboard",
-        element: <Leaderboard
-     />
+        element: <Leaderboard />,
       },
       {
-        path: "/questions/:question_id", 
+        path: "/questions/:question_id",
         element: <PollDetails />,
-        errorElement: <Error />
-      }
-    ]
-  }
+        errorElement: <Error />,
+      },
+    ],
+  },
 ])
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById("root")
+const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>    
+  <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
-    </Provider> 
+    </Provider>
   </React.StrictMode>
-);
+)
 
-
-reportWebVitals();
+reportWebVitals()
